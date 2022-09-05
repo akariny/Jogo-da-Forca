ceeb88cf5b8425efe82bab102cccd5e363753330
Ana Kariny <anakariny10@gmail.com>
2022-09-05 20:05:56 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function mudaImagem() {
+    document.getElementById("imagem-forca").src = "img/perdeu.jpg";
+}
+
+//função para mudar a imagem da forca conforme for errando as letras, ajustar depois
