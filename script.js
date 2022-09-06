c44b74af9ddeab162460ee993a4369ec73a12672
Ana Kariny <anakariny10@gmail.com>
2022-09-06 00:05:29 -0300

    Primeira versão do front-end

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function mudaImagem() {
-    document.getElementById("imagem-forca").src = "img/perdeu.jpg";
+    document.getElementById("imagem-forca").src = "img/forca2.png";
 }
 
 //função para mudar a imagem da forca conforme for errando as letras, ajustar depois
