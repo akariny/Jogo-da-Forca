291bfb1069418ed9ed07313f3f450b20cbf4f382
Ana Kariny <anakariny10@gmail.com>
2022-09-09 01:52:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,27 +2,75 @@
 function mudaImagem() {
     document.getElementById("imagem-forca").src = "img/forca2.png";
 }
+//criando e selecionando a palavra sorteada
+let listaPalavras = ["GATO", "CACHORRO", "CAVALO", "TATU"];
+let palavraSecreta  = "";
 
-document.onkeydown = teclaLetra;
+function escolhePalavraSecreta() {
+    let palavra = listaPalavras[Math.floor(Math.random() * listaPalavras.length)];
+    palavraSecreta = palavra;
+    console.log("a palavra é " + palavraSecreta);
+}
 
-function teclaLetra(e) {
-    const el = document.getElementById(e.key.toUpperCase());
-    if (el) {
-        console.log('apertou ' + e.key);
-        el.style.backgroundColor = 'green';
-        el.style.color = 'white';
-        el.disabled = true;
+//monta a palavra com os traços na tela
+let listaTracos = [];
+
+function montaPalavra() {
+    let espacoPalavra = document.getElementById("palavra-secreta-div");
+
+    for (i = 0; i < palavraSecreta.length; i++) {
+        if(listaTracos[i] == undefined) {
+            listaTracos[i] = "&nbsp;"
+            espacoPalavra.innerHTML = espacoPalavra.innerHTML + "<div class='palavra-secreta-letra'>" + listaTracos[i] + "</div>"
+        } else {
+            espacoPalavra.innerHTML = espacoPalavra.innerHTML + "<div class='palavra-secreta-letra'>" + listaTracos[i] + "</div>"
+        }
     }
 }
 
+//botão novo jogo
+function iniciaJogo() {
+    document.getElementById("div-desaparece").style.display = 'none';
+    document.getElementById("sessao-teclado").style.display = 'block';
+    document.getElementById("palavra-secreta-div").style.visibility = 'visible';
+    
+    escolhePalavraSecreta();
+    montaPalavra();
+}
+
+//botao desistir 
+function desisteJogo() {
+    //zerar o numerode chances e dar game over 
+    
+    //alert("Você voltará para o menu!");
+    //document.getElementById("div-desaparece").style.display = 'block';
+    //document.getElementById("sessao-teclado").style.display = 'none';
+
+    location.reload(true);   
+}
+
+//interações com o teclado
 let teclado = document.getElementsByClassName('botao-teclado');
-teclado.onclick = clicaLetra;
+
+function teclaLetra(e) {
+    const tecla = document.getElementById(e.key.toUpperCase());
+    if (tecla) {
+        console.log('apertou ' + e.key);
+        tecla.style.backgroundColor = 'green';
+        tecla.style.color = 'white';
+        tecla.setAttribute("disabled", "disabled");
+    }
+}
 
 function clicaLetra(e) {
-        let el = document.getElementById(e);
+        let clica = document.getElementById(e);
         console.log('clicou ' + e);
-        el.style.backgroundColor = 'red';
-        el.style.color = 'white';
-        el.disabled = true;   
+        clica.style.backgroundColor = 'red';
+        clica.style.color = 'white';
+        clica.removeAttribute('onclick');
+        clica.setAttribute("disabled", "disabled");  
 }
+
+teclado.onclick = clicaLetra;
+document.onkeydown = teclaLetra;
 
