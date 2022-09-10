c9aff2856f63c78093f07411f505fdccae86b990
Ana Kariny <anakariny10@gmail.com>
2022-09-10 05:14:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-//função para mudar a imagem da forca conforme for errando as letras, ajustar depois
-function mudaImagem() {
-    document.getElementById("imagem-forca").src = "img/forca2.png";
-}
-//criando e selecionando a palavra sorteada
-let listaPalavras = ["GATO", "CACHORRO", "CAVALO", "TATU"];
+let listaPalavras = ["GATO", "CACHORRO", "CAVALO", "TATU", "GAIVOTA", "PAVÃO", "MACACO"];
 let palavraSecreta  = "";
+let listaDinamica = [];
+const letrasCorretas = [];
+const letrasErradas = [];
+let tentativas = 6;
 
+let teclado = document.getElementsByClassName('botao-teclado');
+
+//função para mudar a imagem da forca conforme for errando as letras
+function mudaImagem() {
+    let imagem = ["img/forca1.png", "img/forca2.png", "img/forca3.png", "img/forca4.png", "img/forca5.png", "img/forca6.png"];
+    switch(tentativas) {
+        case 6: document.getElementById("imagem-forca").src = imagem[0];
+        break;
+        case 5: document.getElementById("imagem-forca").src = imagem[1];
+        break;
+        case 4: document.getElementById("imagem-forca").src = imagem[2];
+        break;
+        case 3: document.getElementById("imagem-forca").src = imagem[3];
+        break;
+        case 2: document.getElementById("imagem-forca").src = imagem[4];
+        break;
+        case 1: document.getElementById("imagem-forca").src = imagem[5];
+        break;
+        default: document.getElementById("imagem-forca").src = "img/forca.png";
+        break;
+    }
+}
+
+//criando e selecionando a palavra sorteada
 function escolhePalavraSecreta() {
     let palavra = listaPalavras[Math.floor(Math.random() * listaPalavras.length)];
     palavraSecreta = palavra;
@@ -13,20 +36,23 @@ function escolhePalavraSecreta() {
 }
 
 //monta a palavra com os traços na tela
-let listaTracos = [];
-
 function montaPalavra() {
     let espacoPalavra = document.getElementById("palavra-secreta-div");
 
     for (i = 0; i < palavraSecreta.length; i++) {
-        if(listaTracos[i] == undefined) {
-            listaTracos[i] = "&nbsp;"
-            espacoPalavra.innerHTML = espacoPalavra.innerHTML + "<div class='palavra-secreta-letra'>" + listaTracos[i] + "</div>"
+        if(listaDinamica[i] == undefined) {
+            listaDinamica[i] = "&nbsp;"
+            espacoPalavra.innerHTML = espacoPalavra.innerHTML + "<div class='palavra-secreta-letra'>" + listaDinamica[i] + "</div>"
         } else {
-            espacoPalavra.innerHTML = espacoPalavra.innerHTML + "<div class='palavra-secreta-letra'>" + listaTracos[i] + "</div>"
+            espacoPalavra.innerHTML = espacoPalavra.innerHTML + "<div class='palavra-secreta-letra'>" + listaDinamica[i] + "</div>"
         }
     }
 }
+
+function mostraPalavra() {
+    //fazer
+}
+
 
 //botão novo jogo
 function iniciaJogo() {
@@ -50,25 +76,57 @@ function desisteJogo() {
 }
 
 //interações com o teclado
-let teclado = document.getElementsByClassName('botao-teclado');
-
 function teclaLetra(e) {
     const tecla = document.getElementById(e.key.toUpperCase());
-    if (tecla) {
-        console.log('apertou ' + e.key);
+    
+    if(letrasErradas.includes(e.key.toUpperCase())) {
+        alert("você já escolheu esta letra");
+    } else {
+        if(palavraSecreta.includes(e.key.toUpperCase())) {
+        letrasCorretas.push(e.key.toUpperCase());
         tecla.style.backgroundColor = 'green';
         tecla.style.color = 'white';
         tecla.setAttribute("disabled", "disabled");
+        //desavitar keypress
+        } else {
+            letrasErradas.push(e.key.toUpperCase());
+            tecla.style.backgroundColor = 'red';
+            tecla.style.color = 'white';
+            mudaImagem();
+            tentativas--;
+            }
     }
+
+    
 }
 
 function clicaLetra(e) {
         let clica = document.getElementById(e);
-        console.log('clicou ' + e);
-        clica.style.backgroundColor = 'red';
-        clica.style.color = 'white';
-        clica.removeAttribute('onclick');
-        clica.setAttribute("disabled", "disabled");  
+        console.log('clicou ' + e);  
+
+            if(letrasErradas.includes(e)) {
+                alert("você já escolheu esta letra");
+            } else {
+                if(palavraSecreta.includes(e)) {
+                letrasCorretas.push(e);
+                clica.style.backgroundColor = 'green';
+                clica.style.color = 'white';
+                clica.removeAttribute('onclick');
+                } else {
+                    letrasErradas.push(e);
+                    clica.style.backgroundColor = 'red';
+                    clica.style.color = 'white';
+                    clica.removeAttribute('onclick');
+                    mudaImagem();
+                    tentativas--;
+                    }
+            
+            if(tentativas == 0) {
+                alert("você perdeu :C");
+            }
+        }
+
+        
 }
 
 teclado.onclick = clicaLetra;
